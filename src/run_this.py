83bd7b545817d8e944990a02c5e1ef3cd3989a4f
eina <eooka@teainc.org>
2024-07-23 11:03:52 -0700

    Add a template for executable scripts

---
+++ src/run_this.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python     # Shebang Line - specifies the interpreter to be used
+"""A brief description
+
+Executable scripts containing project entry points should sit directly under the src folder. Filenames should reflect the action taken instead of being called main.py. 
+
+@Author: xxx
+@Maintainer: xxx
+@Date: xxx
+"""
+
+# imports
+
+# config
+
+# logging
+
+# calculation, process or model scripts to run
