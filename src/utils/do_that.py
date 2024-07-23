3d97ee0bafb34d193292459020a0b95932d99bce
eina <eooka@teainc.org>
2024-07-23 11:11:44 -0700

    Add a template script containing methods called from main scripts

---
+++ src/utils/do_that.py
@@ -0,0 +1,25 @@
+"""
+utilsdo_that.py
+
+This module provides utility functions for the project.
+"""
+
+# imports
+
+def do_that (arg1, arg2, ...):
+  """
+  description of do_that
+  
+  Args:
+  arg1 (type): description
+  arg2 (type): description
+  """
+
+  # do that... 
+
+# ==============================
+# Unit Testing
+# ==============================
+
+if __name__ == "__main__":
+    do_that()
